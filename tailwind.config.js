fc9f6654eb6e79df8d6b3464e21c7eecc02b6900
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-10-03 11:12:38 +0200

    basicFramework

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+   content: [
+      "./pages/**/*.{html,js}",
+      "./components/**/*.{html,js}",
+      "./build/**/*.{html,js}",
+   ],
+   theme: {
+      colors: {
+         transparent: "transparent",
+         current: "currentColor",
+         black: colors.black,
+         white: colors.white,
+         gray: colors.gray,
+         emerald: colors.emerald,
+         indigo: colors.indigo,
+         yellow: colors.yellow,
+         orange: colors.orange,
+         green: colors.green,
+      },
+      extend: {},
+   },
+   plugins: [],
+};
